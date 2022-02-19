889c256f5a4009d749c95ac122d031a64294b2db
Deezuleets <daniel.liivak@gmail.com>
2022-02-20 00:42:24 +0200

    Added playerID fetcher

--- main.lua
+++ main.lua
@@ -1,9 +1,16 @@
+local allowGuests = false
+
 function onInit()
-    print("BanMan-Lite Ready")
+    print("BanManager Ready")
     RegisterEvent("onPlayerAuth","onPlayerAuth")
+	RegisterEvent("onChatMessage", "onChatMessage")
 end
 
-function onPlayerAuth(name)
+function onPlayerAuth(name, role, isGuest)
+
+	if isGuest and not allowGuests then
+		return "You must be signed in to join this server!"
+	end
 
 	local f = assert(io.open("../banlist", "r"))
 	local t = f:read ("*all")
@@ -17,4 +24,45 @@ function onPlayerAuth(name)
 	end
 	
 	f:close()
+
+end
+
+function onChatMessage(playerID, senderName, message)
+
+	local f = assert(io.open("../perms", "r"))
+	local t = f:read ("*all")
+
+	local permsMatch = string.match(t, senderName)
+	local getPlayerList = string.match(message, " /idmatch")
+	local messageKick = string.match(message, " /kick")
+	local messageID = string.match(message, "%d+")
+
+	if senderName == permsMatch then
+
+		if message == messageKick then
+			return true
+		end
+		if message == getPlayerList then
+			return true
+		end
+
+		if getPlayerList then
+			local playerName = GetPlayerName(messageID)
+			SendChatMessage(playerID, playerName)
+			return -1
+		end
+
+		if messageKick then
+			if messageID == nil then
+				return "Invalid argument"
+			else
+				DropPlayer(messageID)
+				print("Kicked player with ID " .. messageID)
+				return -1
+			end
+		end
+	end
+
+	f:close()
+
 end

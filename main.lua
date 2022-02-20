202cbd6bd9b715bdeef2e9b7234dc48bf6675ea9
Deezuleets <daniel.liivak@gmail.com>
2022-02-20 13:00:32 +0200

    Fixed code conflict

--- main.lua
+++ main.lua
@@ -2,18 +2,12 @@
 local allowGuests = false
 
 function onInit()
-<<<<<<< Updated upstream
     print("BanManager Ready")
-    RegisterEvent("onPlayerAuth","onPlayerAuth")
-	RegisterEvent("onChatMessage", "onChatMessage")
-=======
-    print("BanManager 1.2.0 Ready")
     MP.RegisterEvent("onPlayerAuth","playerAuthHandler")
 	MP.RegisterEvent("onChatMessage", "chatMessageHandler")
->>>>>>> Stashed changes
 end
 
-function onPlayerAuth(name, role, isGuest)
+function playerAuthHandler(name, role, isGuest)
 
 	if isGuest and not allowGuests then
 		return "You must be signed in to join this server!"
@@ -22,30 +16,30 @@ function onPlayerAuth(name, role, isGuest)
 	local f = assert(io.open("../banlist", "r"))
 	local t = f:read ("*all")
 	
-	print("Checking banlist for", name)
+	print("BanManager: Checking banlist for " .. name)
 	
 	if string.match(t, name) then
 		return "You have been banned from the server."
 	else
-		print("All good, user clear to join.")
+		print("BanManager: All good, user clear to join.")
 	end
 	
 	f:close()
 
 end
 
-function onChatMessage(playerID, senderName, message)
+function chatMessageHandler(playerID, senderName, message)
 
 	local f = assert(io.open("../perms", "r"))
 	local t = f:read ("*all")
 
 	local permsMatch = string.match(t, senderName)
-	local getPlayerList = string.match(message, " /idmatch")
-	local msgKick = string.match(message, " /kick")
-	local msgNum = string.match(message, "%d+")
+	local getPlayerList = string.match(message, "/idmatch")
+	local msgKick = string.match(message, "/kick")
+	local msgNumR = string.match(message, "%d+")
+	local msgNum = tonumber(msgNumR)
 
 	if senderName == permsMatch then
-
 		--=================================--
 		if message == msgKick then
 			return true
@@ -56,15 +50,15 @@ function onChatMessage(playerID, senderName, message)
 		--=================================--
 
 		if getPlayerList then
-			local count = tonumber(msgNum)
+			local count = msgNum
 			while count >= 0 do
-				local playerName = GetPlayerName(count)
+				local playerName = MP.GetPlayerName(count)
 				if playerName == nil then
 					local missingID = "Did not find player with ID " .. count
-					SendChatMessage(playerID, missingID)
+					MP.SendChatMessage(playerID, missingID)
 				else
-					playerName = count .. " - " .. GetPlayerName(count)
-					SendChatMessage(playerID, playerName)
+					playerName = count .. " - " .. MP.GetPlayerName(count)
+					MP.SendChatMessage(playerID, playerName)
 				end
 				count = count - 1
 			end
@@ -75,10 +69,10 @@ function onChatMessage(playerID, senderName, message)
 			if msgNum == nil then
 				return "Invalid argument"
 			else
-				DropPlayer(msgNum)
+				MP.DropPlayer(msgNum)
 				print("Kicked player with ID " .. msgNum)
-				return -1
 			end
+			return -1
 		end
 	end
 

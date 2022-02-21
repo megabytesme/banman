64c54d6b4ade803f35e6adde73385d329b8f3868
Deezuleets <daniel.liivak@gmail.com>
2022-02-21 14:12:32 +0200

    Added /countdown command
    - Added /countdown command
    - Code optimizations

--- main.lua
+++ main.lua
@@ -2,7 +2,7 @@
 local allowGuests = false
 
 function onInit()
-    print("BanManager 1.3.1 Loaded")
+    print("BanManager 1.3.2 Loaded")
     MP.RegisterEvent("onPlayerAuth","playerAuthHandler")
 	MP.RegisterEvent("onChatMessage", "chatMessageHandler")
 end
@@ -23,9 +23,7 @@ function playerAuthHandler(name, role, isGuest)
 	else
 		print("BanManager: All good, user clear to join.")
 	end
-	
 	f:close()
-
 end
 
 function chatMessageHandler(playerID, senderName, message)
@@ -38,28 +36,38 @@ function chatMessageHandler(playerID, senderName, message)
 
 	local permsMatch = string.match(t, senderName)
 	local msgTxt = string.match(message, "%s(.*)")
-	local msgNumR = string.match(message, "%d+")
-	local msgNum = tonumber(msgNumR)
+	local msgNum = tonumber(string.match(message, "%d+"))
 
 	-- Intialize commands
 	local getPlayerList = string.match(message, "/idmatch")
 	local msgKick = string.match(message, "/kick")
 	local msgBan = string.match(message, "/ban")
 	local msgBkick = string.match(message, "/bkick")
+	local msgCountdown = string.match(message, "/countdown")
+
+	if msgCountdown then
+		local i = 3
+		while i > 0 do
+			MP.SendChatMessage(-1, "Countdown: " .. i)
+			i = i - 1
+			MP.Sleep(1000)
+		end
+		MP.SendChatMessage(-1, "Go!")
+		return -1
+	end
 
 	if senderName == permsMatch then
-
 		if getPlayerList then
-			local count = 9
-			while count >= 0 do
-				local playerName = MP.GetPlayerName(count)
+			local i = 9
+			while i >= 0 do
+				local playerName = MP.GetPlayerName(i)
 				if playerName == nil then
 					MP.SendChatMessage(playerID, "Did not find player with ID" .. count)
 				else
-					playerName = count .. " - " .. MP.GetPlayerName(count)
+					playerName = i .. " - " .. MP.GetPlayerName(i)
 					MP.SendChatMessage(playerID, playerName)
 				end
-				count = count - 1
+				i = i - 1
 			end
 			return -1
 		end
@@ -96,8 +104,6 @@ function chatMessageHandler(playerID, senderName, message)
 			return -1
 		end
 	end
-
 	f:close()
 	f2:close()
-
 end
